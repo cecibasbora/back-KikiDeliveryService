9a55ef1f7da6fb8a475145341b3d41763b9f263a
Cecilia Barbosa <barbosacecilia34@gmail.com>
2025-08-08 14:17:04 -0300

    Testing workflow

--- src/infrastructure/connection.ts
+++ src/infrastructure/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-import { DeliveryModel } from './model';
+// import { DeliveryModel } from './model';
 dotenv.config();
 
 export const main = async () => {
@@ -8,11 +8,11 @@ export const main = async () => {
     await mongoose.connect(process.env.MONGODB_URI as string);
     console.log('Servidor online!');
     
-    const result = await DeliveryModel.updateMany(
-      { isDeleted: { $in: [null, undefined] } },
-      { $set: { isDeleted: false } }
-    );
-    console.log(`Data backfilling completo. Foram atualizadas ${result.modifiedCount} entregas.`);
+    // const result = await DeliveryModel.updateMany(
+    //   { isDeleted: { $in: [null, undefined] } },
+    //   { $set: { isDeleted: false } }
+    // );
+    // console.log(`Data backfilling completo. Foram atualizadas ${result.modifiedCount} entregas.`);
   } catch (error) {
     console.error('Erro: ', error);
   }
